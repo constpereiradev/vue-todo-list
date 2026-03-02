0de2f596e0eca32bbfc0b593936e4efc8f3450b2
Amanda Pereira <107977902+constpereiradev@users.noreply.github.com>
2026-03-02 11:40:12 -0300

    Refactor App.vue for task list functionality

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,74 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, computed } from 'vue'
+
+const header = ref('Lista de tarefas')
+
+const items = ref([])
+const newItem = ref('')
+const highPriority = ref(false)
+const newItemLimit = ref(200)
+
+const addItem = () => {
+
+  if (newItem.value.length < 1) {
+    alert('O item deve conter um nome!')
+    return
+  }
+
+  if(newItemCount.value >= newItemLimit.value){
+    alert(`O nome do item não pode ultrapassar ${newItemLimit.value} caracteres`)
+    return
+  }
+
+  var lasItemId = items.value.length == 0 ? 0 : items.value.at(-1).id
+  var newItemId = lasItemId + 1
+
+  items.value.push({
+    id: newItemId,
+    name: newItem.value,
+    highPriority: highPriority.value
+  })
+}
+
+const mark = (item) => {
+  item.marked = true
+}
+
+const removeItem = (index) => {
+  items.value.splice(index, 1)
+}
+
+const newItemCount = computed(() => {
+  return newItem.value.length
+})
+
+const itemsReversed = computed(() => [...items.value].reverse())
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="header">
+    <h1>{{ header }}</h1>
+  </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <form v-on:submit.prevent="addItem">
+    <input v-model="newItem" type="text"></input>
+    <input v-model="highPriority" type="checkbox">Prioridade</input>
+    <button v-bind:disabled="newItem.length === 0 || newItem.length >= newItemLimit">Adicionar</button>
+  </form>
 
-  <main>
-    <TheWelcome />
-  </main>
+  {{newItemCount}}/200
+  <p v-if="!items.length">Nenhum item cadastrado</p>
+  <p v-else>Clique no nome do item para riscar da lista</p>
+
+  <ul> 
+    <li v-for="(item, index) in itemsReversed">
+      <p>
+        <span v-on:click="mark(item)" v-bind:class="{ marked: item.marked, priority: item.highPriority }">{{ item.name }}</span>
+        <button v-on:click="removeItem(index)">Remover</button>
+      </p> 
+
+    </li>
+  </ul>
+
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
